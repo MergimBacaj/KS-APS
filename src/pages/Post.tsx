30ba6e5421765b22fd61eb23f8830d4d29007501
Mergim Bacaj <bacaj.mergim@gmail.com>
2024-02-18 13:42:49 +0100

    add the post component

---
+++ src/pages/Post.tsx
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom";
+import { HandleRequestState } from "../components/HandleRequestState";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+import useGetPost from "../utils/useGetPost";
+import classes from "../styles/Post.module.css";
+
+const Post = () => {
+  const { userId } = useParams();
+  if (!userId) {
+    return <p>loading</p>;
+  }
+  const { loading, post, error } = useGetPost(+userId);
+  return (
+    <div className={classes.postContainer}>
+      <HandleRequestState state={loading} placeholder={<Loading />}>
+        <HandleRequestState state={error} placeholder={<Error />}>
+          <h1 className={classes.postTitle}>{post?.title}</h1>
+          <p className={classes.postBody}>
+            {post?.body ? post.body : " No Posts for this user"}
+          </p>
+          <ul className={classes.postTags}>
+            {post?.tags.map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </ul>
+        </HandleRequestState>
+      </HandleRequestState>
+    </div>
+  );
+};
+
+export default Post;
