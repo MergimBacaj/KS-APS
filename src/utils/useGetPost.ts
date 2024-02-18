85d409147c786174e2677001e13b6e2607100619
Mergim Bacaj <bacaj.mergim@gmail.com>
2024-02-18 13:38:43 +0100

    create a hook for fetching the posts

---
+++ src/utils/useGetPost.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Post {
+  body: string;
+  title: string;
+  id: number;
+  reactions: number;
+  tags: String[];
+  userId: number;
+}
+interface ResponseData {
+  posts: Post[];
+}
+
+const useGetPost = (id: number) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<any | null>(null);
+  const [post, setPost] = useState<Post | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchUsersList = async () => {
+      try {
+        const response = await axios.get<ResponseData>(
+          `https://dummyjson.com/posts`
+        );
+        setPost(response.data.posts.find((post) => post.userId === id));
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsersList();
+  }, []);
+
+  return { loading, error, post };
+};
+
+export default useGetPost;
