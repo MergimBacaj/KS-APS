ee5763bcc92a7dd9b6dd751a344feefbed5bd5d0
Mergim Bacaj <bacaj.mergim@gmail.com>
2024-02-19 10:49:49 +0100

    add time out for the search prop

--- src/utils/useGetUsersList.ts
+++ src/utils/useGetUsersList.ts
@@ -13,30 +13,42 @@ interface GetUsersResponseTypes {
   users: User[];
 }
 
+let searchTimeout: number | undefined | null | NodeJS.Timeout = null;
+
 const useGetUsersList = (props: PropType) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<any | null>(null);
   const [users, setUsers] = useState<GetUsersResponseTypes | null>(null);
 
+  const fetchUsersList = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/users/search", {
+        params: {
+          skip: (props.page - 1) * 20,
+          limit: 20,
+          q: props.search,
+        },
+      });
+      setUsers(response.data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchUsersList = async () => {
-      try {
-        const response = await axios.get("https://dummyjson.com/users/search", {
-          params: {
-            skip: (props.page - 1) * 20,
-            limit: 20,
-            q: props.search,
-          },
-        });
-        setUsers(response.data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
     fetchUsersList();
-  }, [props.page, props.search]);
+  }, [props.page]);
+
+  useEffect(() => {
+    if (searchTimeout) clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(() => {
+      fetchUsersList();
+    }, 500);
+    return () => {
+      if (searchTimeout) clearTimeout(searchTimeout);
+    };
+  }, [props.search]);
 
   return { loading, error, users };
 };
