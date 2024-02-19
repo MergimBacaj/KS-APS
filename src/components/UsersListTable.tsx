15b4fb108616e4b771504202a599951a9b095863
Mergim Bacaj <bacaj.mergim@gmail.com>
2024-02-19 10:46:43 +0100

    Render the users list with useCallback

--- src/components/UsersListTable.tsx
+++ src/components/UsersListTable.tsx
@@ -1,43 +1,48 @@
 import { Link } from "react-router-dom";
 import classes from "../styles/UsersListTable.module.css";
 import { User } from "../utils/user";
+import { useCallback } from "react";
 
 interface PropsType {
   users: User[] | undefined;
 }
 
 const UsersListTable = (props: PropsType) => {
+  const renderUsersList = useCallback((user: User) => {
+    return (
+      <Link
+        to={JSON.stringify(user.id)}
+        key={user.id}
+        className={classes.linkWrapper}
+      >
+        <li className={classes.userInfo}>
+          <h3>{`${user.firstName} ${user.lastName}`}</h3>
+          <>
+            <div>
+              <p className={classes.label}>Email:</p>
+              <p>{user.email}</p>
+            </div>
+            <div>
+              <p className={classes.label}>Company:</p>
+              <p>{user.company.name}</p>
+            </div>
+            <div>
+              <p className={classes.label}>Gender:</p>
+              <p>{user.gender}</p>
+            </div>
+            <div>
+              <p className={classes.label}>University:</p>
+              <p>{user.university}</p>
+            </div>
+          </>
+        </li>
+      </Link>
+    );
+  }, []);
+
   return (
     <ul className={classes.listContainer}>
-      {props.users?.map((user) => (
-        <Link
-          to={JSON.stringify(user.id)}
-          key={user.id}
-          className={classes.linkWrapper}
-        >
-          <li className={classes.userInfo}>
-            <h3>{`${user.firstName} ${user.lastName}`}</h3>
-            <>
-              <div>
-                <p className={classes.label}>Email:</p>
-                <p>{user.email}</p>
-              </div>
-              <div>
-                <p className={classes.label}>Company:</p>
-                <p>{user.company.name}</p>
-              </div>
-              <div>
-                <p className={classes.label}>Gender:</p>
-                <p>{user.gender}</p>
-              </div>
-              <div>
-                <p className={classes.label}>University:</p>
-                <p>{user.university}</p>
-              </div>
-            </>
-          </li>
-        </Link>
-      ))}
+      {props.users?.map(renderUsersList)}
     </ul>
   );
 };
