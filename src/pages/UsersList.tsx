9f6b46d08ec8f259f5ba5abac162bc54ec69e912
Mergim Bacaj <bacaj.mergim@gmail.com>
2024-02-19 10:48:08 +0100

    add useMemo to usersToDisplay

--- src/pages/UsersList.tsx
+++ src/pages/UsersList.tsx
@@ -4,9 +4,10 @@ import Loading from "../components/Loading";
 import Error from "../components/Error";
 import UsersListTable from "../components/UsersListTable";
 import Pagination from "../components/Pagination";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { sortByEmail, sortByName } from "../utils/helpers/SortingFunctions";
 import classes from "./../styles/UsersList.module.css";
+import { User } from "../utils/user";
 
 const UsersList = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -14,24 +15,16 @@ const UsersList = () => {
     undefined
   );
   const [search, setSearch] = useState("");
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+
   const { loading, error, users } = useGetUsersList({
     page: currentPage,
     search: search,
   });
-  if (!users) {
-    return null;
-  }
-  console.log(search);
-  const usersToDisplay =
-    sort === "sortByEmail"
-      ? sortByEmail(users.users)
-      : sort === "sortByName"
-      ? sortByName(users.users)
-      : users.users;
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   const handleSortByName = () => {
     setSort("sortByName");
@@ -50,32 +43,50 @@ const UsersList = () => {
     setCurrentPage(1);
   };
 
+  const usersToDisplay = useMemo<User[]>(() => {
+    if (!users) {
+      return [];
+    }
+
+    let sortedUsers = users.users;
+
+    if (sort === "sortByEmail") {
+      sortedUsers = sortByEmail(sortedUsers);
+    } else if (sort === "sortByName") {
+      sortedUsers = sortByName(sortedUsers);
+    }
+
+    return sortedUsers;
+  }, [users, sort]);
+
   return (
-    <HandleRequestState state={loading} placeholder={<Loading />}>
-      <HandleRequestState state={error} placeholder={<Error />}>
-        <div className={classes.headContainer}>
-          <h1>Users List</h1>
-          <input
-            type="text"
-            onChange={handleSearch}
-            className={classes.search}
-            placeholder="Search..."
-          />
-          <ul className={classes.sortContainer}>
-            <li onClick={handleSortByName}>Sort by Name</li>
-            <li onClick={handleSortByEmail}>Sort by Email</li>
-            <li onClick={handleSortDefault}>Default Sort</li>
-          </ul>
-        </div>
-        <UsersListTable users={usersToDisplay} />
-        {usersToDisplay.length !== 0 && (
-          <Pagination
-            currentPage={currentPage}
-            onPageChange={handlePageChange}
-          />
-        )}
+    <div>
+      <div className={classes.headContainer}>
+        <h1>Users List</h1>
+        <input
+          type="text"
+          onChange={handleSearch}
+          className={classes.search}
+          placeholder="Search..."
+        />
+        <ul className={classes.sortContainer}>
+          <li onClick={handleSortByName}>Sort by Name</li>
+          <li onClick={handleSortByEmail}>Sort by Email</li>
+          <li onClick={handleSortDefault}>Default Sort</li>
+        </ul>
+      </div>
+      <HandleRequestState state={loading} placeholder={<Loading />}>
+        <HandleRequestState state={error} placeholder={<Error />}>
+          <UsersListTable users={usersToDisplay} />
+          {usersToDisplay?.length !== 0 && (
+            <Pagination
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+            />
+          )}
+        </HandleRequestState>
       </HandleRequestState>
-    </HandleRequestState>
+    </div>
   );
 };
 
